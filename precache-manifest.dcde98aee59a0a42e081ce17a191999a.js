3c953f9d6c7e0dbfa7a632d539ab8390d96a47c4
Marcos Mantovani <mvmes23@gmail.com>
2021-10-08 17:12:05 -0300

    Updates

---
+++ precache-manifest.dcde98aee59a0a42e081ce17a191999a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c33565bf2163f550f263d50b534e74",
+    "url": "/mvmes2/TrybeWallet.git/Home/index.html"
+  },
+  {
+    "revision": "80f81a623132fc3c8b65",
+    "url": "/mvmes2/TrybeWallet.git/Home/static/css/main.74ead237.chunk.css"
+  },
+  {
+    "revision": "680c01dfd5c70503d8a6",
+    "url": "/mvmes2/TrybeWallet.git/Home/static/js/2.c8c89e4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mvmes2/TrybeWallet.git/Home/static/js/2.c8c89e4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80f81a623132fc3c8b65",
+    "url": "/mvmes2/TrybeWallet.git/Home/static/js/main.51359164.chunk.js"
+  },
+  {
+    "revision": "79f4fcbe557449f63d2f",
+    "url": "/mvmes2/TrybeWallet.git/Home/static/js/runtime-main.ecc68097.js"
+  },
+  {
+    "revision": "111b2e519f82b6ef94644008606d50aa",
+    "url": "/mvmes2/TrybeWallet.git/Home/static/media/logo.111b2e51.png"
+  }
+]);
